2430beb19d5d0de445f423cad52994a765b25f4f
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-08-02 07:25:20 -0400

    Update CodeFormat.js

--- Template/CodeFormat.js
+++ Template/CodeFormat.js
@@ -1,3 +1,12 @@
-{
-	"folders": []
-}
+function imprimirElemento(elemento){
+	var ventana = window.open('', 'PRINT', 'height=400,width=600');
+	ventana.document.write('<html><head><title>' + document.title + '</title>');
+	ventana.document.write('</head><body >');
+	ventana.document.write(elemento.innerHTML);
+	ventana.document.write('</body></html>');
+	ventana.document.close();
+	ventana.focus();
+	ventana.print();
+	ventana.close();
+	return true;
+  }
